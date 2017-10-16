yarnpkg/yarn

Write remote.resolved prop to package.json as _resolved key
This is needed to Esy to get unique id of where the package comes from (in case
of git dep the version only isn't enough). This is also what npm does by the
way.

src/package-linker.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import type {Manifest} from './types.js';
+import type {Manifest, PackageRemote} from './types.js';
 import type PackageResolver from './package-resolver.js';
 import type {Reporter} from './reporters/index.js';
 import type Config from './config.js';
@@ -124,7 +124,7 @@ export default class PackageLinker {
     const phantomFiles = [];
 
     //
-    const queue: Map<string, CopyQueueItem> = new Map();
+    const queue: Map<string, {remote: ?PackageRemote, item: CopyQueueItem}> = new Map();
     for (const [dest, {pkg, loc: src}] of flatTree) {
       const ref = pkg._reference;
       invariant(ref, 'expected package reference');
@@ -138,12 +138,15 @@ export default class PackageLinker {
       }
 
       queue.set(dest, {
-        src,
-        dest,
-        onFresh() {
-          if (ref) {
-            ref.setFresh(true);
-          }
+        remote: metadata.remote,
+        item: {
+          src,
+          dest,
+          onFresh() {
+            if (ref) {
+              ref.setFresh(true);
+            }
+          },
         },
       });
     }
@@ -183,9 +186,11 @@ export default class PackageLinker {
       }
     }
 
+    const linkTasks = Array.from(queue.values());
+
     //
     let tick;
-    await fs.copyBulk(Array.from(queue.values()), this.reporter, {
+    await fs.copyBulk(linkTasks.map((item) => item.item), this.reporter, {
       possibleExtraneous,
       phantomFiles,
 
@@ -204,6 +209,16 @@ export default class PackageLinker {
         }
       },
     });
+
+    // inject PackageRemote info into installation dep tree
+    await Promise.all(linkTasks.map(async ({remote, item: {dest}}) => {
+      const packageJsonFilename = path.join(dest, 'package.json');
+      const packageJson = await fs.readJson(packageJsonFilename);
+      if (remote != null && remote._resolved != null) {
+        packageJson._resolved = remote.resolved;
+      }
+      await fs.writeJson(packageJsonFilename, packageJson);
+    }));
 
     // remove any empty scoped directories
     for (const scopedPath of scopedPaths) {

esy-ocaml/esy-install

src/package-linker.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import type {Manifest} from './types.js';
+import type {Manifest, PackageRemote} from './types.js';
 import type PackageResolver from './package-resolver.js';
 import type {Reporter} from './reporters/index.js';
 import type Config from './config.js';
@@ -160,7 +160,7 @@ export default class PackageLinker {
     // list of artifacts in modules to remove from extraneous removal
     const artifactFiles = [];
 
-    const copyQueue: Map<string, CopyQueueItem> = new Map();
+    const copyQueue: Map<string, {item: CopyQueueItem, remote: any}> = new Map();
     const hardlinkQueue: Map<string, CopyQueueItem> = new Map();
     const hardlinksEnabled = linkDuplicates && (await fs.hardlinksWork(this.config.cwd));
 
@@ -222,14 +222,17 @@ export default class PackageLinker {
           copiedSrcs.set(src, dest);
         }
         copyQueue.set(dest, {
-          src,
-          dest,
-          type,
-          onFresh() {
-            if (ref) {
-              ref.setFresh(true);
-            }
+          item: {
+            src,
+            dest,
+            type,
+            onFresh() {
+              if (ref) {
+                ref.setFresh(true);
+              }
+            },
           },
+          remote
         });
       } else {
         hardlinkQueue.set(dest, {
@@ -341,9 +344,11 @@ export default class PackageLinker {
       }
     }
 
+    const linkTasks = Array.from(copyQueue.values());
+
     //
     let tick;
-    await fs.copyBulk(Array.from(copyQueue.values()), this.reporter, {
+    await fs.copyBulk(linkTasks.map((item) => item.item), this.reporter, {
       possibleExtraneous,
       artifactFiles,
 
@@ -379,6 +384,16 @@ export default class PackageLinker {
       this.reporter.verbose(this.reporter.lang('verboseFileRemoveExtraneous', loc));
       await fs.unlink(loc);
     }
+
+    // inject PackageRemote info into installation dep tree
+    await Promise.all(linkTasks.map(async ({remote, item: {dest}}) => {
+      const packageJsonFilename = path.join(dest, 'package.json');
+      const packageJson = await fs.readJson(packageJsonFilename);
+      if (remote != null && remote.resolved != null) {
+        packageJson._resolved = remote.resolved;
+      }
+      await fs.writeJson(packageJsonFilename, packageJson);
+    }));
 
     // remove any empty scoped directories
     for (const scopedPath of scopedPaths) {
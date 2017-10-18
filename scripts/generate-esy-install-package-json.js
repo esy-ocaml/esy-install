1ff9e543e81fddfe61719339034b97d7df9153fe
Andrey Popp <8mayday@gmail.com>
2017-10-18 15:02:22 +0300

    Release process
    * Renamed package to `@esy-ocaml/esy-install`, this is the name we are
      going to publish it under.
    * Added `@esy-ocaml/esy-opam` to externals in webpack config so the
      bundled version will have it excluded.
    * Added `scripts/generate-esy-install-package-json.js` which generates
      minimal `package.json` with a single dependency on `@esy-ocaml/esy-opam`.
    * Command `make build-release` makes a ready to publish npm package in
      `packages/esy-install`.
    * Command `make publish` publishes built package on npm.

---
+++ scripts/generate-esy-install-package-json.js
@@ -0,0 +1,23 @@
+/** @flow */
+
+const path = require("path");
+const fs = require("fs");
+
+const packageJsonFilename = require.resolve("../package.json");
+const packageJson = JSON.parse(fs.readFileSync(packageJsonFilename, "utf8"));
+
+const releasePackageJson = {
+  name: packageJson.name,
+  version: packageJson.version,
+  license: packageJson.license,
+  description: packageJson.description,
+  dependencies: {
+    "@esy-ocaml/esy-opam": packageJson.dependencies["@esy-ocaml/esy-opam"]
+  },
+  engines: packageJson.engines,
+  repository: packageJson.repository,
+  bin: packageJson.bin,
+  files: ["bin/"]
+};
+
+console.log(JSON.stringify(releasePackageJson, null, 2));

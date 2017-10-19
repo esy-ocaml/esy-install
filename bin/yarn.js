8c836414b7d5e4790ff3315f9bcb4296c2c44af7
Andrey Popp <8mayday@gmail.com>
2017-10-19 14:37:29 +0300

    Fix requireing dev code

--- bin/yarn.js
+++ bin/yarn.js
@@ -11,12 +11,18 @@ if (majorVer < 4) {
   console.error('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.');
   process.exitCode = 1;
 } else {
-  var dirPath = '../lib/';
-  var v8CompileCachePath = dirPath + 'v8-compile-cache';
   var fs = require('fs');
-  // We don't have/need this on legacy builds and dev builds
-  if (fs.existsSync(v8CompileCachePath)) {
-    require(v8CompileCachePath);
+  var path = require('path');
+  var dirPath;
+  if (fs.existsSync(path.join(__dirname, '..', 'src'))) {
+    dirPath = '../src/';
+  } else {
+    dirPath = '../lib/';
+    var v8CompileCachePath = dirPath + 'v8-compile-cache';
+    // We don't have/need this on legacy builds and dev builds
+    if (fs.existsSync(v8CompileCachePath)) {
+      require(v8CompileCachePath);
+    }
   }
 
   // Just requiring this package will trigger a yarn run since the

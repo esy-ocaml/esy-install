34fd8e30c151b53f5d8330940409df283d094174
Bryan Phelps <bryphe@outlook.com>
2018-07-04 18:05:14 -0700

    Revert '-p0' change, as the bug wasn't with the patch call - but that 'esy-bash' wasn't respected the 'cwd' passed in

--- src/fetchers/opam-fetcher.js
+++ src/fetchers/opam-fetcher.js
@@ -240,7 +240,7 @@ async function applyPatches(dest, patches) {
     await fs.writeFile(patchFilename, patch.content, {encoding: 'utf8'});
     try {
       if (isWindows) {
-          await bashExec(`patch -p0 -i ${patchFilename}`, {
+          await bashExec(`patch -p1 -i ${patchFilename}`, {
             cwd: dest,
             stdio: 'inherit',
           });
